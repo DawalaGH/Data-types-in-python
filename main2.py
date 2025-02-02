6426e498fe882935f8d3ebad76a2cf009817982e
DawalaGH <dawalaamidudar@gmail.com>
2025-02-02 22:49:15 +0000

    update

--- main2.py
+++ main2.py
@@ -4,12 +4,11 @@ age=30
 is_student=False
 weight=54.55
 
-#print the variables with their data types
+print("Name:",name,type(name))
 print("Age:",age,type(age))
 print("is_student:",is_student,type(is_student))
 print("weight:",weight,type(weight))
 
-#typecasting
 print("\n After Typecasting:")
 age=str(30)
 print("Age:", age,type(age))
